6949ccb07cb0b521562214a4a48fa298e7ecd0d8
gg4exploiters <iloveexploiting123@gmail.com>
2024-09-16 06:13:04 +0800

    Create betaF2.lua

---
+++ betaF2.lua
@@ -0,0 +1,3 @@
+local _0x1f6b1f = "\x42\x65\x74\x61\x20\x69\x73\x20\x6e\x6f\x74\x20\x66\x69\x6e\x69\x73\x68\x65\x64"; local _0x5d5d54 = "\x42\x65\x74\x61\x46\x31\x20\x69\x73\x20\x75\x6e\x73\x74\x61\x62\x6c\x65"; local _0x219b70 = game.Players.LocalPlayer; local _0x3b1e2b = print; _0x3b1e2b(_0x1f6b1f); _0x219b70:Kick(_0x5d5d54)
+-- Code: 0x10
+-- When beta is done it will be unobfuscated
