09e1475f44158e81546768860ea28af9ff436806
Liquid <iloveexploiting123@gmail.com>
2024-09-24 20:51:13 +0800

    Update Debug.lua

--- lib/Debug.lua
+++ lib/Debug.lua
@@ -4,28 +4,31 @@ local upvaluesRegistry = {}
 
 function debug.setupupvalues(func, upvalues)
     if type(func) ~= "function" then
-        error("argument must be a function")
+        error("Argument must be a function")
     end
     upvaluesRegistry[func] = upvalues
 end
 
 function debug.getupvalues(func)
     if type(func) ~= "function" then
-        error("argument must be a function")
+        error("Argument must be a function")
     end
     return upvaluesRegistry[func] or {}
 end
 
-function debug.getupvalue(modulepath, funcname, index)
-    local module = require(modulepath)
-    local func = module[funcname]
+function debug.getupvalue(modulePath, funcName, index)
+    local module = require(modulePath)
+    local func = module[funcName]
 
     if not func or type(func) ~= "function" then
-        error("the function " .. tostring(funcname) .. " is not valid in the required module")
+        error("The function " .. tostring(funcName) .. " is not valid in the required module")
     end
 
     local upvalues = debug.getupvalues(func)
-    return upvalues[index]
+    if index and index > 0 and index <= #upvalues then
+        return upvalues[index]
+    end
+    error("Invalid index")
 end
 
 return debug
