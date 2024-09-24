c554fd9733eca51326bc8429ee6058a46fb65888
Liquid <iloveexploiting123@gmail.com>
2024-09-24 21:01:33 +0800

    Update Debug.lua

--- lib/Debug.lua
+++ lib/Debug.lua
@@ -2,6 +2,24 @@ local debug = {}
 
 local upvaluesRegistry = {}
 
+function debug.getupvalue(modulePath, funcName, index)
+    local status, module = pcall(require, modulePath)
+    if not status then
+        error("Failed to require module: " .. tostring(modulePath))
+    end
+
+    local func = module[funcName]
+    if not func or type(func) ~= "function" then
+        error("The function " .. tostring(funcName) .. " is not valid in the required module")
+    end
+
+    local upvalues = debug.getupvalues(func)
+    if index and index > 0 and index <= #upvalues then
+        return upvalues[index]
+    end
+    error("Invalid index or upvalue does not exist")
+end
+
 function debug.setupvalues(func, upvalues)
     if type(func) ~= "function" then
         error("Argument must be a function")
@@ -16,7 +34,7 @@ function debug.getupvalues(func)
     return upvaluesRegistry[func] or {}
 end
 
-function debug.getupvalue(modulePath, funcName, index)
+function debug.getupvalue3(modulePath, funcName, index)
     local status, module = pcall(require, modulePath)
     if not status then
         error("Failed to require module: " .. tostring(modulePath))
