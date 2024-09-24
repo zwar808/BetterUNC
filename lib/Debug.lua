1a7ca25f5a50598626d424d7ca740ae85b7bcb27
Liquid <iloveexploiting123@gmail.com>
2024-09-24 20:23:33 +0800

    Update Debug.lua

--- lib/Debug.lua
+++ lib/Debug.lua
@@ -1,37 +1,31 @@
 local Debug = {}
 
-function Debug.getupvalues(func)
+function Debug.getUpvalues(func, index)
     if type(func) ~= "function" then
         error("Argument must be a function")
     end
-    local upvalues = {}
-    local index = 1
-    while true do
-        local name, value = debug.getupvalue(func, index)
-        if not name then break end
-        upvalues[name] = value
-        index = index + 1
+    if index then
+        return debug.getupvalue(func, index)
+    else
+        local upvalues = {}
+        local i = 1
+        while true do
+            local name, value = debug.getupvalue(func, i)
+            if not name then break end
+            upvalues[name] = value
+            i = i + 1
+        end
+        return upvalues
     end
-    return upvalues
 end
 
-function Debug.getupvalue(func, index)
-    if type(func) ~= "function" then
-        error("Argument must be a function")
-    end
-    local name, value = debug.getupvalue(func, index)
-    return value
-end
-
-function Debug.requireUpvalue(path, functionName, upvalueIndex)
-    local module = require(path)
+function Debug.requireUpvalue(modulePath, functionName, upvalueIndex)
+    local module = require(modulePath)
     local func = module[functionName]
-
     if not func or type(func) ~= "function" then
         error("The function " .. tostring(functionName) .. " is not valid in the required module")
     end
-
-    return Debug.getupvalue(func, upvalueIndex)
+    return Debug.getUpvalues(func, upvalueIndex)
 end
 
 return Debug
